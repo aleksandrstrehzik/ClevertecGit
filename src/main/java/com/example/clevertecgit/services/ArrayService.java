8ab9c14bcff2c128a8e92997614a366449e03730
-LENOVO- <aleksandrstrehzik@gmail.com>
2023-02-07 21:30:12 +0300

    feat: (created a new service with a method returning a List<String>)

---
+++ src/main/java/com/example/clevertecgit/services/ArrayService.java
@@ -0,0 +1,13 @@
+package com.example.clevertecgit.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArrayService {
+
+    public List<String> getArrayList() {
+        return List.of("a", "b", "c");
+    }
+}
