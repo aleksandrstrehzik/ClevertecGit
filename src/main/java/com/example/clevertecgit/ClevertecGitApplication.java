bd76abfd964cc049633f80f9644865c463b36d1f
-LENOVO- <aleksandrstrehzik@gmail.com>
2023-02-07 20:52:15 +0300

    create: (new project to practice with git)

---
+++ src/main/java/com/example/clevertecgit/ClevertecGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.clevertecgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClevertecGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClevertecGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clevertecgit/ClevertecGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clevertecgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClevertecGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
